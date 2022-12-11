aa67a229c7a36cc553b8d33ef20eec111df3e600
swim0413 <chosuyoung0413@gmail.com>
2022-12-11 22:18:53 +0900

    로그인 회원가입 게스트 로그인

--- routes/login.js
+++ routes/login.js
@@ -3,38 +3,29 @@ const router = express.Router();
 const fs = require('fs');
 
 let db = null;
-if(!fs.existsSync('./data/db.json')){
+if(!fs.existsSync('./data/users/db.json')){
     let initial = JSON.parse('{"data": []}');
-    fs.writeFile('./data/db.json', JSON.stringify(initial, null, 2), 'utf-8', function(err){
+    fs.writeFile('./data/users/db.json', JSON.stringify(initial, null, 2), 'utf-8', function(err){
         if(err){
             console.log(err);
         }
     })
 }
-fs.readFile('./data/db.json', 'utf-8', (err, jsonFile)=>{
+fs.readFile('./data/users/db.json', 'utf-8', (err, jsonFile)=>{
     db = JSON.parse(jsonFile);
 });
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'login' });
+    if(!req.session.user) res.render('login', { title: 'login' });
+    else res.redirect('/main');
 });
-
-// let users = {
-//     "data":[
-//         {
-//             'id': 'asdf',
-//             'pw': 'aa',
-//         },
-//         {
-//             'id': 'fd',
-//             'pw': 'ss',
-//         },
-//     ]
-// }
 
 router.post('/', (req, res) => {
     let id = req.body.login_id;
     let pw = req.body.login_pw
+    let dbIndex = db['data'].findIndex(obj => obj.id == id);
+    if(id.trim()=="" || pw.trim()=="") return res.redirect('/');
+    
     console.log(id, pw)
     let ids = [];
     let pws = [];
@@ -43,39 +34,63 @@ router.post('/', (req, res) => {
         pws.push(element['pw']);
     }
     if(!ids.includes(id)){
+        res.send('없는 아이디 입니다');
         res.redirect('/');
     }else if(!pws.includes(pw)){
+        res.send('비밀번호가 틀렸습니다');
         res.redirect('/');
     }else{
         if (req.session.user) {
             // 세션에 유저가 존재한다면
             console.log("이미 로그인 돼있습니다~");
             res.redirect('/main');
-          } else {
+        } else {
             req.session.user = {
-              id: id,
-              pw: pw,
-              name: "null",
-              authorized: true,
+                id: id,
+                pw: pw,
+                nickName: db['data'][dbIndex]['nickName'],
+                authorized: true,
+                isGuest: false,
             };
             console.log(req.session.user);
             res.redirect('/main');
-          }
+        }
+    }
+});
+
+router.post('/guestLogin', (req, res)=>{
+    if (req.session.user) {
+        // 세션에 유저가 존재한다면
+        console.log("이미 로그인 돼있습니다~");
+        res.redirect('/main');
+    } else {
+        req.session.user = {
+            id: null,
+            pw: null,
+            nickName: 'Guest',
+            authorized: true,
+            isGuest: true,
+        };
+        console.log(req.session.user);
+        res.redirect('/main');
     }
 });
 
 router.post('/register', (req, res)=>{
     let id = req.body.register_id;
     let pw = req.body.register_pw;
+    let nickName = req.body.nickName
+    if(id.trim()=="" || pw.trim()=="" || nickName.trim()=="") return res.redirect('/');
     let ids = [];
     for(const element of db['data']){
         ids.push(element['id']);
     }
     if(ids.includes(id)){
+        res.send('중복된 아이디');
         res.redirect('/');
     }else{
-        db['data'].push({id,pw});
-        fs.writeFile('./data/db.json', JSON.stringify(db, null, 2), 'utf-8', function(err){
+        db['data'].push({id,pw,nickName});
+        fs.writeFile('./data/users/db.json', JSON.stringify(db, null, 2), 'utf-8', function(err){
             if(err){
                 console.log(err);
             }
--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,23 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(!req.session.user) res.redirect('/');
-    else res.render('main', { title: req.session.user['id'] });
+    else res.render('main', { title: req.session.user['nickName'] });
 });
+
+router.post('/logout', function(req, res){
+    if(req.session.user){
+        console.log('로그아웃');
+        
+        req.session.destroy(function(err){
+            if(err) throw err;
+            console.log('세션삭제후 로그아웃');
+            res.redirect('/');
+        });
+    }
+    else{
+        console.log('로그인 상태 아님');
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
