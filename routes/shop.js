4c65a0ace716645c059e4a396edfb9ff26d9198b
swim0413 <chosuyoung0413@gmail.com>
2023-02-03 23:46:51 +0900

    2023/2/3상품이름클릭가능ㅋ

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb');
 const connect = require('../db/connect.js');
 
 async function makeDB(title, base64_img, price, desc){
@@ -32,14 +33,37 @@ async function getDB(){
     }
     return res;
 }
-console.log(getDB())
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     let products = await getDB();
     products = JSON.stringify(products);
     if(!req.session.user) res.redirect('/');
-    else res.render('shop', { userName: req.session.user['nickName'], products: JSON.parse(products)});
+    else{
+        if(req.query.product){
+            let product = await getDBById(req.query.product);
+            product = JSON.stringify(product);
+            return res.render('productInfo', { userName: req.session.user['nickName'], product: JSON.parse(product)});
+        } 
+        else res.render('shop', { userName: req.session.user['nickName'], products: JSON.parse(products)});
+    } 
+    
 });
+
+async function getDBById(_id){
+    let connection;
+    let res;
+    try{
+        connection = await connect();
+        const db  = await connection.db('shop');
+        const products = db.collection('products');
+        res = await products.findOne({_id:new mongodb.ObjectID(_id)});
+    }catch(e){
+        console.log(e.message);
+    }finally{
+        connection.close();
+    }
+    return res;
+}
 
 module.exports = router;
